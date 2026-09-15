ab188840b0fa6315882ebb2187ad3f01e77faa6d
edwin <edwinkun@gmail.com>
2026-09-15 12:46:38 +0700

    initial commit :pig2: :fish: :dog2:

---
+++ src/main/java/com/edw/Main.java
@@ -0,0 +1,17 @@
+package com.edw;
+
+/**
+ * <pre>
+ *  com.edw.Main
+ * </pre>
+ *
+ * @author Muhammad Edwin < edwin at redhat dot com >
+ * 15 Sept 2026 12:46
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
